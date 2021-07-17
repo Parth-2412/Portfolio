492a551f48d9585b2db2ad345cb790a5d8acf0fb
Parth Patil <parthnpatil06@gmail .com>
2021-07-17 18:02:24 +0530

    Add hover effect

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
 function Logo() {
 	return (
-		<div className="text-2xl font-semibold cursor-pointer">
+		<div className="text-2xl font-semibold cursor-pointer hover:opacity-80">
 			<a href="/">Parth2412</a>
 		</div>
 	);
