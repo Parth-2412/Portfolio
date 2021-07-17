aba964126eca7648aa0ce0815c5e60add05ec32f
Parth Patil <parthnpatil06@gmail .com>
2021-07-17 13:46:59 +0530

    Make catchphrase more readable and make button text a link

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 				{/* <link rel="icon" href="/favicon.ico" /> */}
 			</Head>
 			<div className="flex items-center p-12">
-				<div className="space-y-9 max-w-xl">
+				<div className="space-y-9 max-w-prose">
 					<div className="space-y-4">
 						<div className="text-2xl font-medium">
 							Hi, my name is Parth
@@ -19,7 +19,7 @@ export default function Home() {
 						</div>
 					</div>
 					<button className="flex space-x-2 items-center rounded-lg text-lg text-primary hover:text-white hover:bg-opacity-90 hover:bg-primary border-2 border-primary py-2 px-4">
-						<p>Let's talk</p>
+						<a href="/#contact">Let's talk</a>
 						<ArrowRightIcon className="w-5" />
 					</button>
 				</div>
