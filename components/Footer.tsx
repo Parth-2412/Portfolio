3d8a3dd86806d561bcc6166f4e10400eef83f5fd
Parth Patil <parthnpatil06@gmail .com>
2021-07-21 12:36:37 +0530

    Add space between elements in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
 	return (
 		<div className="bg-gray-100 text-gray-800 text-sm py-8 absolute left-0 right-0 mt-10  justify-center items-center flex">
 			<div className="flex flex-grow max-w-7xl justify-center">
-				<p>© 2021 Parth2412. All rights reserved</p>
+				<p>© 2021 Parth2412.&nbsp;&nbsp; All rights reserved</p>
 			</div>
 		</div>
 	);
